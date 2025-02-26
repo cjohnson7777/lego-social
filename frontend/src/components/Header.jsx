f0f55829084cf1c233f2478f7ba1a0e4155d8a44
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2025-02-26 00:12:27 -0600

    more frontend added tailwind

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,85 @@
 
+import {
+    BookmarkIcon,
+    CircleStackIcon,
+    UserIcon,
+  } from '@heroicons/react/20/solid'
+  import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
-function Header(){
+  
+  export default function Header() {
     return (
-        <div>
-            <img src="#" alt="logo" />
-            <h1>Lego Social</h1>
-            <input type="text" />
-
-            <ul>
-                <li>Profile</li>
-                <li>Saved</li>
-                <li>Collection</li>
-            </ul>
+      <div className="lg:flex lg:items-center lg:justify-between m-3">
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+            Lego Social
+          </h2>
+          <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+           
+          </div>
         </div>
+        <div className="mt-5 flex lg:mt-0 lg:ml-4">
+          <span className="hidden sm:block">
+            <button
+              type="button"
+              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50"
+            >
+              <BookmarkIcon aria-hidden="true" className="mr-1.5 -ml-0.5 size-5 text-gray-400" />
+              Saved
+            </button>
+          </span>
+  
+          <span className="ml-3 hidden sm:block">
+            <button
+              type="button"
+              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50"
+            >
+              <CircleStackIcon aria-hidden="true" className="mr-1.5 -ml-0.5 size-5 text-gray-400" />
+              Collection
+            </button>
+          </span>
+  
+          <span className="sm:ml-3">
+            <button
+              type="button"
+              className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <UserIcon aria-hidden="true" className="mr-1.5 -ml-0.5 size-5" />
+              Profile
+            </button>
+          </span>
+        {/*
+          Dropdown
+           <Menu as="div" className="relative ml-3 sm:hidden">
+            <MenuButton className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:ring-gray-400">
+              More
+              <ChevronDownIcon aria-hidden="true" className="-mr-1 ml-1.5 size-5 text-gray-400" />
+            </MenuButton>
+  
+            <MenuItems
+              transition
+              className="absolute right-0 z-10 mt-2 -mr-1 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+            >
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                >
+                  Edit
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                >
+                  View
+                </a>
+              </MenuItem>
+            </MenuItems>
+          </Menu> */}
+        </div>
+      </div>
     )
-}
-
-export default Header;
+  }
+  
